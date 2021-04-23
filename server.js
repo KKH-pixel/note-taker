e7aef2696e992c1ad1bdebf94f473bc95f3bb0e7
KKH-pixel <kendra.hunt@seattlecentral.edu>
2021-04-22 21:47:09 -0700

    initial base code for program

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+let data = require('./db/db.json');
+const shortid = require('shortid');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3000;
+
+// body parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// delivery of user side info
+app.use(express.static('public'))
+
+// Basic route that sends the user first to the AJAX Page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+
+
+app.get('/api/notes', (req, res) => res.json(data));
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    console.log(shortid.generate());
+    const note = {
+        title: req.body.title,
+        text: req.body.text,
+        id: shortid.generate()
+    } 
+    data.push(note)
+    fs.writeFile('./db/db.json', JSON.stringify(data), (err) => {
+  err ? console.error(err) : console.log('Success!');
+  res.json(data)
+    });
+});
+
+
+
+// app.get('/api/notes', (req, res) => res.json(data));
+
+
+
+// GET * should return the index.html file. place last
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
